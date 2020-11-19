1e96753c03393f5545e9a4f3c94ddd85899eb877
Emanuel Rangel <emanuelrangelluna@gmail.com>
2020-11-18 19:27:09 -0600

    318 componentes de carga

--- src/app/services/carga-imagenes.service.ts
+++ src/app/services/carga-imagenes.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 
 import { AngularFirestore } from 'angularfire2/firestore';
 import * as firebase from 'firebase';
+import { FileItem } from '../models/file-item';
 
 
 @Injectable({
@@ -12,6 +13,11 @@ export class CargaImagenesService {
   private CARPETA_IMAGENES = 'img';
 
   constructor(private db: AngularFirestore) { }
+
+   cargarImagenesFirebase(imagenes: FileItem[]){
+      console.log(imagenes);
+      
+   } 
 
   private guardarImage (imagen: {nombre: string, url: string} ) {
     this.db.collection(`/${this.CARPETA_IMAGENES}`)
