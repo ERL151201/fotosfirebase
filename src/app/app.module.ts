7efc416b3ea9f7a94177167bea26949df199b71f
Emanuel Rangel <emanuelrangelluna@gmail.com>
2020-11-18 18:50:32 -0600

    317 Configuracion de Firebase y Servicio de carga

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,12 @@ import { AppComponent } from './app.component';
 import { FotosComponent } from './components/fotos/fotos.component';
 import { CargaComponent } from './components/carga/carga.component';
 import { APP_ROUTES } from './app.routes';
+import { CargaImagenesService } from './services/carga-imagenes.service';
+
+import { AngularFireModule } from 'angularfire2';
+import { environment } from 'src/environments/environment';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { AngularFireAuthModule } from 'angularfire2/auth';
 
 @NgModule({
   declarations: [
@@ -14,9 +20,14 @@ import { APP_ROUTES } from './app.routes';
   ],
   imports: [
     BrowserModule,
-    APP_ROUTES
+    APP_ROUTES,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule, // imports firebase/firestore, only needed for database features
+    AngularFireAuthModule
   ],
-  providers: [],
+  providers: [
+    CargaImagenesService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCJgoIHY812OftG2z7d9G1lNQeBPEWzM0I',
+    authDomain: 'fir-fotos-73ad5.firebaseapp.com',
+    databaseURL: 'https://fir-fotos-73ad5.firebaseio.com',
+    projectId: 'fir-fotos-73ad5',
+    storageBucket: 'fir-fotos-73ad5.appspot.com',
+    messagingSenderId: '293935152158',
+    appId:'1:293935152158:web:02ee533fc7d80d7032020f',
+    measurementId: 'G-RZWQP3WXY4'
+  }
 };
 
 /*
